f73038fa796d67d8454fcfe1c5ec35a30796e0f7
zl <492242843@qq.com>
2022-06-22 14:51:52 +0800

    完成文章管理相关功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,7 +4,13 @@
 $.ajaxPrefilter(function (options) {
     // console.log(options.url)
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = 'http://www.liulongbin.top:3007' + options.url
+    // options.url = 'http://www.liulongbin.top:3007' + options.url
+    // http://big-event-api-t.itheima.net
+    // options.url = 'http://big-event-api-t.itheima.net' + options.url
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    http://api-breakingnews-web.itheima.net
+  
+   
     // console.log(options.url)
     // 统一为有权限的接口，设置headers 请求头
     if(options.url.indexOf('/my/') !== -1){
